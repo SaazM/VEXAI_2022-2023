adc2fe40932463b0eb2112c952a363c0bcbd2e46
kingKedSha <kedaar.s@gmail.com>
2022-07-30 11:14:39 -0400

    python grab_data camera

--- nano/yolo/grab_data.py
+++ nano/yolo/grab_data.py
@@ -17,13 +17,14 @@ config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
 # Start streaming
 pipeline.start(config)
-
+n = 0
 try:
     while True:
 
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
+        
         depth_frame = frames.get_depth_frame()
         if not depth_frame or not color_frame:
             print("hello there")
@@ -32,7 +33,8 @@ try:
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
-
+        if(n % 30 == 0): cv2.imwrite('./frames/temp' + str(int(n/30)) + '.jpg', color_image)
+        
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
@@ -51,6 +53,7 @@ try:
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', images)
         cv2.waitKey(1)
+        n+=1
 
 finally:
 
